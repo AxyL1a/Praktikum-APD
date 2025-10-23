e24f620d90da5fa1edbc82a0a9c7d28e14ba45b1
AxyL1a <denimulia735@gmail.com>
2025-10-23 08:51:02 +0800

    pertemuan7

---
+++ Kelas/pertemuan-7/pertemuan-7.py
@@ -0,0 +1,34 @@
+# def perkenalan():
+#     print ("HAI SEMUANYA")
+#     print ("APA KABAR?")
+# perkenalan()
+
+# def LuasPersegiPanjang(panjang, lebar):
+#     luas = panjang * lebar
+#     print (f"luas dari persegi panjang adalah {luas}")
+# LuasPersegiPanjang(7, 8)
+
+# def luaspersegi(sisi):
+#     luas = sisi*sisi
+#     return luas
+# def volume_persegi (sisi):
+#     volume = luaspersegi(sisi) * sisi
+#     print("Volume Persegi : ", volume)
+# luaspersegi(9)
+# volume_persegi(6)
+
+# def faktorial(n):
+#     if n == 1 or n == 0:
+#         return 1
+
+#     else:
+#         return n * faktorial(n - 1)
+# hasil = faktorial(9)
+# print(f"Hasil dari 9! adalah: {hasil}")
+
+# try:
+#     usn = input('Username yang diinginkan : ')
+#     if len(usn) < 5:
+#         raise ValueError('Nama Minimal Memiliki 5 karakter')
+# except ValueError as e:
+#     print(e)
